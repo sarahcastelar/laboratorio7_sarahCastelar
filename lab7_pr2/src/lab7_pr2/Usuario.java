61254f65d228ca50b36b6056447c8a81473fe80d
sarahcastelar <sarahgcastelar@gmail.com>
2018-06-08 13:21:24 -0600

    clase usuarios creado

---
+++ lab7_pr2/src/lab7_pr2/Usuario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab7_pr2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Usuario {
+    
+    private String correo;
+    private String contrasena;
+    private String fecha;
+    private ArrayList <Pelicula> peliculas = new ArrayList();
+    private ArrayList <Serie> series = new ArrayList();
+    private String tarjeta;
+    
+    
+}
